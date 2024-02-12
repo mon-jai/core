d8f7831d46b7d36e7039b166ff80758301195162
mon-jai <91261297+mon-jai@users.noreply.github.com>
2024-02-13 04:00:02 +0800

    Format code

--- src/execute.ts
+++ src/execute.ts
@@ -13,15 +13,17 @@ import {settings} from './settings'
  * @example For ESM dev.js
  * ```
  * #!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
- * import { execute } from "@oclif/core"
- *
+ * 
+ * import {execute} from '@oclif/core'
+ * 
  * await execute({development: true, dir: import.meta.url})
  * ```
  *
  * @example For ESM run.js
  * ```
  * #!/usr/bin/env node
- * import { execute } from "@oclif/core"
+ *
+ * import {execute} from '@oclif/core'
  *
  * await execute({dir: import.meta.url})
  * ```
